75314b857c375d9432700057a5c10d83550d8a15
Brian <Brian@DESKTOP-O6A53S7>
2016-06-09 13:39:08 -0500

    Primer COmit

---
+++ tutorial UPSNET/MainPage.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Microsoft.ProjectOxford.Vision;
+using Microsoft.ProjectOxford.Vision.Contract;
+using Windows.Storage.Pickers;
+using Windows.Storage;
+using System.Diagnostics;
+using System.Text;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace tutorial_UPSNET
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void btn_BuscaImagen_Click(object sender, RoutedEventArgs e)
+        {
+            VisionServiceClient VisionServiceClient = new VisionServiceClient("6c4c5da4f0374952abd0b1a1e024d33f");
+            FileOpenPicker openPicker = new FileOpenPicker();
+            openPicker.ViewMode = PickerViewMode.Thumbnail;
+            openPicker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+            openPicker.FileTypeFilter.Add(".jpg");
+            openPicker.FileTypeFilter.Add(".png");
+            openPicker.FileTypeFilter.Add(".bmp");
+            StorageFile file = await openPicker.PickSingleFileAsync();
+            if (file != null)
+            {
+                var stream = await file.OpenAsync(Windows.Storage.FileAccessMode.Read);
+                Stream imageFileStream = stream.AsStreamForRead();
+                OcrResults ocrResult = await VisionServiceClient.RecognizeTextAsync(imageFileStream, "es");
+                Debug.WriteLine(ocrResult.ToString());
+                ExtraeOCR(ocrResult);
+            }
+         }
+
+        protected void ExtraeOCR(OcrResults results)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            if (results != null && results.Regions != null)
+            {
+                stringBuilder.AppendLine();
+                foreach (var item in results.Regions)
+                {
+                    foreach (var line in item.Lines)
+                    {
+                        foreach (var word in line.Words)
+                        {
+                            stringBuilder.Append(word.Text);
+                            stringBuilder.Append(" ");
+                        }
+                        stringBuilder.AppendLine();
+                    }
+                    stringBuilder.AppendLine();
+                }
+            }
+            Debug.WriteLine(stringBuilder.ToString());
+        }
+
+
+    }
+}
